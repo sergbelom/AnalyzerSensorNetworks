7e9b899ace6b3a8efe90a0b1496afc124b9f1877
kirilka <kir.1@mail.ru>
2018-01-20 19:40:03 +0300

    1.1.1.0 Renamed Form1 to FormSafeCity

--- SafeCity/SafeCity/FormSafeCity.Designer.cs
+++ SafeCity/SafeCity/FormSafeCity.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SafeCity
 {
-    partial class Form1
+    partial class FormSafeCity
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSafeCity));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.connectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
--- SafeCity/SafeCity/FormSafeCity.cs
+++ SafeCity/SafeCity/FormSafeCity.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SafeCity
 {
-    public partial class Form1 : Form
+    public partial class FormSafeCity : Form
     {
-        public Form1()
+        public FormSafeCity()
         {
             InitializeComponent();
         }
